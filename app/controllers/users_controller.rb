dc5e1378f8f783fd983249c5c0cb350bcc568bfc
Putt2020 <6230067421@student.chula.ac.th>
2021-10-13 21:42:50 +0700

    Decorate all pages and include javascript for delete data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    puts "--------------call show #{@user.id}-----------------"
   end
 
   # GET /users/new
@@ -50,6 +51,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
+    puts "--------------call delete #{@user.id}-----------------"
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
